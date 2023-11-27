5e530fbecc861f53a1157520e963e70a07b7d783
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-11-27 12:20:17 +0500

    appDbContext added

---
+++ Apartment.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Apartment.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Apartment.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        
+        public virtual DbSet<Person> People { get; set; }
+        public virtual DbSet<House> Houses { get; set; }
+    }
+}

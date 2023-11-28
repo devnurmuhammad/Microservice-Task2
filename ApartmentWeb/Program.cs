4e5b34a8d5f8f80ff7c8bfdab88e2f1b14089486
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-11-28 18:29:33 +0500

    fixed

--- ApartmentWeb/Program.cs
+++ ApartmentWeb/Program.cs
@@ -1,5 +1,8 @@
+using Apartment.Application.Services.Persons;
 using Apartment.Infrastructure.Data;
+using Apartment.Infrastructure.Repositories.Persons;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,11 +12,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//System.Net.ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+builder.Services.AddScoped<IPersonRepository, PersonRepository>();
+builder.Services.AddScoped<IPersonService, PersonService>();
+// This line should be added before making the SQL connection
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AnotherConnectionString"));
+     options.UseSqlServer(builder.Configuration.GetConnectionString("AnotherConnectionString"), options =>
+      {
+          options.EnableRetryOnFailure(); // Add this line to enable retry on failure
+      });
 });
+   
 
 var app = builder.Build();
 
